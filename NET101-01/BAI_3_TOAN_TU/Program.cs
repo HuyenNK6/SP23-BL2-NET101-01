9a9f5588ab608e871fb66aa75185d80a42d81381
huyennk6 <huyennk6@fpt.edu.vn>
2023-03-10 16:33:03 +0700

    bai 4-5 toan tu- vong lap

---
+++ NET101-01/BAI_3_TOAN_TU/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAI_4_TOAN_TU
+{
+    internal class Program
+    {
+        /*
+         * 4 Loại toán tử trong lập trình
+         *  1. Toán tử số học:
+         *  (+ - * / % ++ --)
+         *      +: Tính tổng của 2 số hoặc dùng để cộng chuỗi
+         *      -: Phép trừ của 2 số
+         *      *: Phép nhân của 2 số
+         *      %: Phép chia lấy dư của 2 số
+         *      ++: Tăng lên 1 đơn vị
+         *      --: Giảm 1 đơn vị
+         *  2. Toán tử so sánh:
+         *  (> < >= <= == !=)
+         *      >: Lớn hơn
+         *      <: nhỏ hơn
+         *      >=: lớn hơn hoặc bằng
+         *      <=: nhỏ hơn hoặc bằng
+         *      ==: so sánh bằng
+         *      !=: so sánh khác
+         *  3. Toán tử Logic:
+         * (&& || !)
+         *      &&: Yêu cầu các mệnh đề phải luôn đúng
+         *      ||: Yêu cầu 1 trong các mệnh đề dúng là được
+         *      !: Lấy giá trị phủ định của mệnh đề.
+         *  4. Toán tử gán:
+         * (= += -= *= /= %=)
+         *      +=: Cộng tiếp với vế bên phải các phép còn lại tương tự
+         */
+
+        /*
+         * Toán tử ba ngôi
+         *  <biểu thức> ? <kết quả đúng> : <kết quả sai>
+         */
+        static void Main(string[] args)
+        {
+            //Nhập xuất thông tin của máy tính
+            //ma: string
+            //hang: string
+            //gia: double
+            //inch: float
+            //win: int
+            //isNew: bool => true/ false
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+            //NHẬP =>    cw tab tab
+            Console.WriteLine("-----NHẬP THÔNG TIN------");
+            //ma: string
+            Console.Write("Nhập mã: ");
+            string ma = Console.ReadLine();
+            //hang: string
+            Console.Write("Nhập hãng: ");
+            string hang = Console.ReadLine();
+            //gia: double
+            Console.Write("Nhập giá tiền: ");
+            double gia = Convert.ToDouble(Console.ReadLine());
+            //inch: float
+            Console.Write("Nhập inch: ");
+            float inch = (float)Convert.ToDouble(Console.ReadLine());
+            //win: int
+            Console.Write("Nhập win: ");
+            int win = Convert.ToInt32(Console.ReadLine());
+            //isNew: bool => true/ false
+            Console.Write("Máy mới hay cũ (true/false)? ");
+            bool isNew = Convert.ToBoolean(Console.ReadLine());
+            //XUẤT
+            Console.WriteLine("------XUẤT THÔNG TIN----------");
+            Console.WriteLine($"ma = {ma}");
+            Console.WriteLine($"hang = {hang}");
+            Console.WriteLine($"gia = {gia}");
+            Console.WriteLine($"inch = {inch}");
+            Console.WriteLine($"win = {win}");
+            Console.WriteLine($"isNew = {isNew}");
+            Console.WriteLine($"isNew = {(isNew == true ? "Máy mới" : "Máy cũ")}");
+
+            /////////
+            //(isNew) => true
+
+            if (!isNew)
+            {
+                Console.WriteLine("Máy mới");
+            }
+            else
+            {
+                Console.WriteLine("Máy cũ");
+            }
+
+
+            ///Console.Read(); Đọc 1 ký tự từ bàn phím nhưng là số ASCII
+            Console.WriteLine("Có muốn tiếp tục không? ");
+            int tiepTuc = Console.Read();
+            Console.WriteLine(tiepTuc);
+
+        }
+    }
+}
---
+++ NET101-01/BAI_5_CAU_TRUC_RE_NHANH/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAI_5_IF_ELSE_VONG_LAP
+{
+    internal class Program
+    {
+        /*
+        * Bài Vòng lặp:
+        *
+        * Định nghĩa chung cho tất cả vòng lặp:
+        * Vòng lặp cho phép lặp lại một hoặc nhiều câu lệnh cho đến khi thỏa mãn điều kiện được chỉ định định.
+        *
+        * Các loại vòng lặp: while, do..while, for, foreach
+        *
+        * Từ khóa sử dụng trong vòng lặp:
+        *
+        * break - ngắt vòng lặp
+        * continue: Sẽ bỏ qua các đoạn code phía sau nó và nó quay trở lại lần lặp tiếp theo.
+        *
+        * Vòng lặp while: Chỉ chạy khi điều kiện luôn đúng và nếu điều kiện luôn đúng ko điểm dừng thì được gọi là vòng lặp vô hạn.
+        * + Cách dùng: wh + tab
+        *
+        * + Công thức:
+        *  while (true)
+           {
+               //Thực thi 1 câu lệnh hoặc nhiều câu lệnh
+           }
+           Vòng lặp Do..While: Thực hiện hành động ít nhất là 1 lần. Điều kiện đề vòng lặp chạy giống như While(True).
+          Cách gõ: do + tab
+         
+        Công thức:
+          do {
+                     //Thực hiện hành động ít nhất là 1 lần.
+          } while (true);
+       */
+        static void Main(string[] args)
+        {
+            //1. VÒNG LẶP FOR
+            //for tab
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"i= {i}");
+            }
+            Console.WriteLine("Nhap so luong n= ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine($"i= {i}");
+                }
+            }
+            //while
+            //whi tab
+            int a = 0;
+            //giả n > 0 rồi
+            while (a < n)
+            {
+                Console.WriteLine("Co Huyen xinh gai "+a);
+                a++;
+            }
+            int b = 0;
+            while (b < 10) 
+            {
+                if(b == 5)
+                {
+                    Console.WriteLine("Co Huyen ette "+ b);
+                    //break;
+                    b++;
+                    continue;
+                }
+                Console.WriteLine("Co Huyen de thuong "+b);
+                b++;
+            }
+
+            //do while
+            //do tab
+            int c = 50;
+            do
+            {
+                Console.WriteLine(c+ " Co Huyen hien nhat BM");
+                c++;
+            } while (c < 20);
+        }
+    }
+}/*
+      * Bài 1: Nhập và xuất thông tin Xe Máy
+      * - id: int
+      * - hang: string
+      * - gia: double
+      * - phanKhoi: int
+      * - isVietNam: bool 
+      * => yêu cầu in ra true là Xe Máy Việt Nam/ false là Xe Máy Nước Ngoài
+      * 
+      * Bài 2: Nhập min- max (int) từ bàn phím
+      * - Nếu người dùng nhập min/max <0 yêu cầu nhập lại
+      * - In ra tất cả các số lẻ trong khoảng min-max
+      * - và đếm xem có bao nhiêu số lẻ đó
+      */
